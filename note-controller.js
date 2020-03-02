488e532f39ad37c48c477218f005b78926c3d399
Richard Pattinson <richie.pattinson@googlemail.com>
2020-03-02 16:40:48 +0000

    Step 6 spiked, no tests written

--- note-controller.js
+++ note-controller.js
@@ -1,8 +1,15 @@
+(function(exports){
+    function NoteListController(noteListModel) {
+        this.app = document.getElementById('app');
+        noteList.createNote("Favourite drink: kombucha")
+        this.view = new NoteListView(noteListModel)
+    }
+    NoteListController.prototype.updateContent = function(){
+        this.app.innerHTML = this.view.listHTML()
+    }
+    exports.NoteListController = NoteListController;
+})(this);
 
-var app = document.getElementById('app');
-console.log(app)
-function updateContent(){
-    app.innerText = "howdy"
-}
-
-updateContent()
+var noteList = new NoteList()
+var noteListController = new NoteListController(noteList);
+noteListController.updateContent()
