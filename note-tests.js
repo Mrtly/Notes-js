90e36d67760c4a2525080357863f070de6960dd5
Richard Pattinson <richie.pattinson@googlemail.com>
2020-03-02 14:27:49 +0000

    add noteList object and test createNote method

--- note-tests.js
+++ note-tests.js
@@ -4,9 +4,9 @@ james= "blah blah";
 richard="blah blah blah blah";
 
 function noteIncludesText(contentText) {
+    console.log("noteIncludesText")
     var note = new Note(contentText);
     assert.isTrue(note.content === contentText);
-    console.log("Your note content is: " + note.content);
   };
   
   noteIncludesText(richard);
