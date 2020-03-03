b7d0e89699121b80a4b61bb9e3be3980d8d8f1eb
Richard Pattinson <richie.pattinson@googlemail.com>
2020-03-03 14:13:54 +0000

    add link to each note sending to #noteID url

--- note-list-model.js
+++ note-list-model.js
@@ -1,11 +1,12 @@
 (function (exports) {
     function NoteList() {
-        this.count = 0
+        this.count = 0;
         this.content = [];
     }
+
     NoteList.prototype.add = function (note) {
-        note.id = this.count
-        this.count++
+        note.id = this.count;
+        this.count++;
         this.content.push(note)
     };
     NoteList.prototype.createNote = function (text) {
--- note-list-view.js
+++ note-list-view.js
@@ -5,11 +5,12 @@
     NoteListView.prototype.listHTML = function () {
         var newarr = [];
         for (var i = 0; i < this.content.content.length; i++) {
+            var noteID = this.content.content[i].id
             if(this.content.content[i].content.length > 20) {
-                newarr.push("<ul><li><div>" + this.content.content[i].content.substring(0, 20) +"..." + "</div></li></ul>")
+                newarr.push(`<ul><li><div><a href=#notes/${noteID}> ${this.content.content[i].content.substring(0, 20)}...</a></div></li></ul>`)
             }
             else {
-                newarr.push("<ul><li><div>" + this.content.content[i].content + "</div></li></ul>")
+                newarr.push(`<ul><li><div><a href=#notes/${noteID}> ${this.content.content[i].content}</a></div></li></ul>`)
                 }
         }
         return newarr.join('')
@@ -18,9 +19,4 @@
 
     exports.NoteListView = NoteListView;
 })(this);
-
-// var noteList2 = new NoteList()
-// noteList2.createNote("I went to the shops with ben last week and had a really fun adventure")
-// var noteListView2 = new NoteListView(noteList2)
-// console.log(noteListView2.listHTML())
 
