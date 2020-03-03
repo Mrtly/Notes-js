d246f3298843590af314e4c4b712eb4f4123ebf5
Richard Pattinson <richie.pattinson@googlemail.com>
2020-03-03 16:46:58 +0000

    can see whole note but must extract hardcoded notes

--- note-controller.js
+++ note-controller.js
@@ -2,11 +2,43 @@
     function NoteListController(noteListModel) {
         this.app = document.getElementById('app');
         noteListModel.createNote("Favourite Drink: Kombucha")
+        noteListModel.createNote("Its a whole new Note")
         this.view = new NoteListView(noteListModel)
     }
     NoteListController.prototype.updateContent = function(){
         this.app.innerHTML = this.view.listHTML()
     }
+
+    NoteListController.prototype.makeUrlChangeShowNoteForCurrentPage = function() {
+        window.addEventListener("hashchange", () => this.showNoteForCurrentPage());
+    };
+
+    NoteListController.prototype.showNoteForCurrentPage = function() {
+        this.showNote(this.getNoteFromUrl(window.location));
+    };
+
+    NoteListController.prototype.getNoteFromUrl = function(location) {
+        return location.hash.split("#notes/")[1];
+    };
+
+    NoteListController.prototype.getContent = function(noteID){
+
+        for (var i = 0; i < this.view.content.content.length; i++) {
+            if(noteID == this.view.content.content[i].id){
+                return this.view.content.content[i].content
+            }else {return "No matches"}
+        }
+    }
+
+    NoteListController.prototype.showNote = function(noteID) {
+        document
+            .getElementById("app")
+            .innerHTML = this.getContent(noteID);
+    };
+
     exports.NoteListController = NoteListController;
 })(this);
 
+let noteList = new NoteList()
+let controller = new NoteListController(noteList)
+controller.makeUrlChangeShowNoteForCurrentPage()
