b9d36c2b04805bbbc0c0b72bf9985570e286f0e6
Richard Pattinson <richie.pattinson@googlemail.com>
2020-03-04 12:38:52 +0000

    submit button added with event listener to prevent default

--- note-controller.js
+++ note-controller.js
@@ -1,45 +1,60 @@
-(function(exports){
+(function (exports) {
     function NoteListController(noteListModel) {
         this.app = document.getElementById('app');
-        noteListModel.createNote("Favourite Drink: Kombucha")
-        noteListModel.createNote("Its a whole new Note")
+        noteListModel.createNote("Favourite Drink: Kombucha");
+        noteListModel.createNote("Its a whole new Note");
         this.view = new NoteListView(noteListModel)
     }
-    NoteListController.prototype.updateContent = function(){
-        this.app.innerHTML = this.view.listHTML()
-    }
 
-    NoteListController.prototype.makeUrlChangeShowNoteForCurrentPage = function() {
+    NoteListController.prototype.updateContent = function () {
+        this.app.innerHTML = this.view.listHTML()
+    };
+
+    NoteListController.prototype.makeUrlChangeShowNoteForCurrentPage = function () {
         window.addEventListener("hashchange", () => this.showNoteForCurrentPage());
     };
 
-    NoteListController.prototype.showNoteForCurrentPage = function() {
+    NoteListController.prototype.showNoteForCurrentPage = function () {
         this.showNote(this.getNoteFromUrl(window.location));
     };
 
-    NoteListController.prototype.getNoteFromUrl = function(location) {
+    NoteListController.prototype.getNoteFromUrl = function (location) {
         return location.hash.split("#notes/")[1];
     };
 
-    NoteListController.prototype.getContent = function(noteID){
+    NoteListController.prototype.getContent = function (noteID) {
 
         for (var i = 0; i < this.view.content.content.length; i++) {
-            console.log(this.view.content.content[i].id)
-            if(noteID == this.view.content.content[i].id){
+            console.log(this.view.content.content[i].id);
+            if (noteID == this.view.content.content[i].id) {
                 return this.view.content.content[i].content
             }
         }
-    }
+    };
 
-    NoteListController.prototype.showNote = function(noteID) {
+    NoteListController.prototype.showNote = function (noteID) {
         document
             .getElementById("app")
             .innerHTML = this.getContent(noteID);
+    };
+    NoteListController.prototype.createNote = function(text){
+        this.view.content.createNote(text)
+    }
+    NoteListController.prototype.listenForSubmit = function () {
+        form = document.getElementById("new_note")
+        form.addEventListener("submit", (event) =>{
+            event.preventDefault();
+        })
+        console.dir(form)
     };
 
     exports.NoteListController = NoteListController;
 })(this);
 
-let noteList = new NoteList()
-let controller = new NoteListController(noteList)
-controller.makeUrlChangeShowNoteForCurrentPage()
+let noteList = new NoteList();
+let controller = new NoteListController(noteList);
+// controller.createNote("Favourite Drink: Kombucha");
+// controller.createNote("Its a whole new Note");
+controller.updateContent()
+controller.listenForSubmit();
+controller.makeUrlChangeShowNoteForCurrentPage();
